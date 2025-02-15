e89f2b890c8889e28a5a1b586f8533f049bbeb69
kaeeraa <kaeeraa@nebula-nook.ru>
2025-02-15 15:30:25 +0300

    feat(routes): add paths
    Signed-off-by: kaeeraa <kaeeraa@nebula-nook.ru>

---
+++ src/kochan/routes/__init__.py
@@ -0,0 +1,6 @@
+from os.path import join, dirname
+
+static_path = join(dirname(__file__), "..", "static")
+images_path = join(static_path, "images")
+css_path = join(static_path, "css")
+fonts_path = join(static_path, "fonts")
