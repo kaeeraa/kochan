f6f576de0963d2fa0d4b80b6bf5102391f6da317
kaeeraa <kaeeraa@nebula-nook.ru>
2025-02-15 15:30:44 +0300

    feat(index): update root route
    Signed-off-by: kaeeraa <kaeeraa@nebula-nook.ru>

--- src/kochan/routes/index.py
+++ src/kochan/routes/index.py
@@ -1,8 +1,22 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, send_from_directory
+from kochan.routes import css_path
 
 index_bp = Blueprint("index", __name__)
+
+#############
+# HTML file #
+#############
 
 
 @index_bp.route("/")
 def index() -> str:
-    return "Hello, world of kochan!"
+    return render_template("index.html")
+
+############
+# CSS file #
+############
+
+
+@index_bp.route("/static/index.css")
+def index_css():
+    return send_from_directory(css_path, "index.css")
